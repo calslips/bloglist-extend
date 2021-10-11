6769c32562419013aa9d8ec946c584441d6d8721
Caleb Phillips <csphillips5@gmail.com>
2021-10-11 14:33:19 -0400

    route user to home after successful blog deletion

--- src/components/BlogDeletion.js
+++ src/components/BlogDeletion.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { setNotice } from '../reducers/noticeReducer';
 import { deleteBlog } from '../reducers/blogReducer';
 
 const BlogDeletion = ({ blog, forceLogout }) => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const removeBlog = async () => {
     try {
       if (window.confirm(`Remove blog '${blog.title}' by ${blog.author}`)) {
         await dispatch(deleteBlog(blog.id));
+        history.push('/');
         dispatch(setNotice(`Removed blog '${blog.title}' by ${blog.author}`, 5, 'success'));
       }
     } catch (exception) {
